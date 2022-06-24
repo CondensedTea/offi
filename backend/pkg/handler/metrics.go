c35267feea9845f9144cbdd6aa73e786e4951581
Alex Tyshkevich <alex@tyshkevich.ru>
2022-06-25 01:26:23 +0400

    backend: fixed offi_client_version_requests_counter not being registered

--- backend/pkg/handler/metrics.go
+++ backend/pkg/handler/metrics.go
@@ -9,11 +9,13 @@ var clientVersionCounter = prometheus.NewCounterVec(
 	prometheus.CounterOpts{
 		Name: "offi_client_version_requests_counter",
 		Help: "Counter for client version in request",
-	},
-	[]string{"version"},
-)
+	}, []string{"version"})
+
+func init() {
+	prometheus.MustRegister(clientVersionCounter)
+}
 
 func clientVersionMiddleware(ctx *fiber.Ctx) error {
-	clientVersionCounter.With(prometheus.Labels{"version": ctx.Params("version")}).Inc()
+	clientVersionCounter.WithLabelValues(ctx.Params("version")).Inc()
 	return ctx.Next()
 }
