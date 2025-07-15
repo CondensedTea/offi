d63d10a2d9fc739beca1fe9d2e6de3455473064e
Alex Tyshkevich <alex@tyshkevich.ru>
2025-07-15 18:44:23 +0200

    backend: fix SaveMatchExists returning redis.Nil as error

--- backend/internal/redis/matches.go
+++ backend/internal/redis/matches.go
@@ -2,12 +2,20 @@ package redis
 
 import (
 	"context"
+	"errors"
 	"strconv"
 	"time"
+
+	"github.com/redis/go-redis/v9"
 )
 
 func (r Client) SaveMatchExists(ctx context.Context, matchID int) error {
-	return r.client.Set(ctx, strconv.Itoa(matchID), 1, 7*24*time.Hour).Err()
+	err := r.client.Set(ctx, strconv.Itoa(matchID), 1, 7*24*time.Hour).Err()
+	if errors.Is(err, redis.Nil) {
+		return nil
+	}
+
+	return err
 }
 
 func (r Client) MatchExists(ctx context.Context, matchID int) (bool, error) {
